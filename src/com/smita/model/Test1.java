e61cda74b25554d00f00f6efed7726da995eac35
Smita16101985 <smitakambleud@gmail.com>
2020-03-22 23:15:38 +0530

    Update Test1.java

--- src/com/smita/model/Test1.java
+++ src/com/smita/model/Test1.java
@@ -3,7 +3,8 @@ package com.smita.model;
 public class Test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("manish");
+    		System.out.println("Hello");
 
 	}
 
